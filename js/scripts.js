31eb280aa98a998947e3fe2bf176844e8c8ed72e
David Boedigheimer <stealth259@gmail.com>
2019-11-12 15:46:59 -0800

    Add 5 arrays, reworking loop into if statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 // Back end
 var romanNumbers = function(numberInput) {
-  var romans = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-  var decimals = [1, 5, 10, 50, 100, 500, 1000];
-  var splitNumbers = numberInput.split(" ");
-  var returnNumber = []
+  // var romans = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+  // var decimals = [1, 5, 10, 50, 100, 500, 1000];
+  // Used in the previous pass.
+  var ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+  var tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+  var hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+  var thousands = ['', 'M', 'MM', 'MMM'];
+  var romans = [ones, tens, hundreds, thousands];
 
+  var splitNumbers = numberInput.split("").reverse();
+  var returnNumber = [];
 
-  for (var i = 0; i <decimals.length; i++) {
-    if (decimals[i] == splitNumbers) {
-      returnNumber.push(romans[i]);
-    }
+  if (splitNumber.length === 4) {
+    returnNumber.push()
   }
-  return returnNumber.join("");
+
+  // for (var i = 0; i < splitNumbers.length; i++) {
+  //   console.log(splitNumbers, romans);
+  //   if (splitNumbers[i] == 1) {
+  //     returnNumber.push(ones[1]);
+  //     console.log(romans[i]);
+  //   }
+  // }
+  return returnNumber.reverse().join("");
 }
 // Front end
 $(document).ready(function() {
