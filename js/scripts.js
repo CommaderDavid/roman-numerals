65c89c24284acf5ce0a5b6fc7fe627822567e30d
David Boedigheimer <stealth259@gmail.com>
2019-11-12 10:56:18 -0800

    Add 2nd and 3rd pass, match numbers to sympols.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
 // Back end
 var romanNumbers = function(numberInput) {
   var romans = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-  var splitNumbers = numberInput.split("");
+  var decimals = [1, 5, 10, 50, 100, 500, 1000];
+  var splitNumbers = numberInput.split(" ");
   var returnNumber = []
 
 
+  for (var i = 0; i <decimals.length; i++) {
+    if (decimals[i] == splitNumbers) {
+      returnNumber.push(romans[i]);
+    }
+  }
+  return returnNumber.join("");
 }
 // Front end
 $(document).ready(function() {
   $("form#roman").submit(function(e) {
     e.preventDefault();
 
-    var numberInput = parseInt($("input#numbers").val());
+    var numberInput = $("input#numbers").val();
     var result = romanNumbers(numberInput);
 
     $("#results").append(result + "<br>");
